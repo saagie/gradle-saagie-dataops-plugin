d7f52d2b7c501002394e9771785e52a961092cc4
Renan Decamps <renan.decamps@gmail.com>
2019-06-21 11:23:08 +0200

    feat: setup

---
+++ bin/test/io/saagie/plugin/DataOpsPluginTest.groovy
@@ -0,0 +1,38 @@
+package io.saagie.plugin
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class DataOpsPluginTest extends Specification {
+    @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+    List pluginClasspath
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+        pluginClasspath = getClass().classLoader
+            .findResource('plugin-classpath.txt')
+            .readLines()
+            .collect {
+                new File(it)
+            }
+    }
+
+    def "hello task should print 'Hello World'"() {
+        given:
+            buildFile << """
+            """
+            Project project = ProjectBuilder.builder().build()
+            project.pluginManager.apply(DataOpsPlugin.class)
+
+        when:
+            def taskResult = project.tasks.getByName('hello')
+
+        then:
+            println(taskResult)
+    }
+}
