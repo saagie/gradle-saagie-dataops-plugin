c5be50cc1082275f4fc713e2e1d6bd396fd6e322
Renan Decamps <renan.decamps@gmail.com>
2019-07-03 18:55:29 +0200

    test: add ok test for projectList

--- src/test/groovy/io/saagie/plugin/DataOpsPluginTest.groovy
+++ src/test/groovy/io/saagie/plugin/DataOpsPluginTest.groovy
@@ -1,5 +1,7 @@
 package io.saagie.plugin
 
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Rule
@@ -9,6 +11,8 @@ import spock.lang.*
 class DataOpsPluginTest extends Specification {
     @Rule
     TemporaryFolder testProjectDir = new TemporaryFolder()
+
+    MockWebServer mockWebServer = new MockWebServer()
 
     File buildFile
 
@@ -32,6 +36,74 @@ class DataOpsPluginTest extends Specification {
         gradle(true, arguments)
     }
 
+
+    // ===========================
+    // TESTS =====================
+    def "projectList task should return a list of project"() {
+        given:
+        def mockedResponse = new MockResponse()
+        mockedResponse.setResponseCode(200)
+        mockedResponse.body = """
+            {
+                "data": {
+                    "projects": [
+                        {
+                            "id": "8321e13c-8de2a-4481-8552-5be4b6cc5df4",
+                            "name": "Test new Project",
+                            "creator": "simon.chmel",
+                            "description": "Test Simon",
+                            "jobsCount": 2,
+                            "status": "READY"
+                        },
+                        {
+                            "id": "7f5e0374-0c45-45a3-a2f3-bdlezdezd40f876fa",
+                            "name": "Test Spark config",
+                            "creator": "test.dezdzek",
+                            "description": "",
+                            "jobsCount": 1,
+                            "status": "READY"
+                        },
+                        {
+                            "id": "dezdzjx-7b7f-44f0-9f69-cd377152a2e9",
+                            "name": "For tests",
+                            "creator": "test.name",
+                            "description": "Pour Test Name",
+                            "jobsCount": 7,
+                            "status": "READY"
+                        },
+                        {
+                            "id": "5b781e22-dzdioz-4831-8a03-deiozjdiz",
+                            "name": "Test R 6.8.9",
+                            "creator": "test.test",
+                            "description": "",
+                            "jobsCount": 0,
+                            "status": "READY"
+                        }
+                    ]
+                }
+            }
+        """
+        buildFile << """
+            saagie {
+                server {
+                    url = 'http://localhost:9000'
+                    login = 'fake.user'
+                    password = 'ThisPasswordIsWrong'
+                    environment = 2
+                }
+            }
+        """
+        mockWebServer.enqueue(mockedResponse)
+        mockWebServer.start(9000)
+
+        when:
+        def result = gradle('projectList')
+
+        then:
+        !result.output.contains('"data"')
+        result.output.contains('[{"id":"8321e13c')
+    }
+
     def "projectList task with bad config should fail"() {
         given:
         buildFile << """
@@ -47,7 +119,6 @@ class DataOpsPluginTest extends Specification {
 
         when:
         def result = gradle('projectList')
-        println result.output
 
         then:
         thrown(Exception)
