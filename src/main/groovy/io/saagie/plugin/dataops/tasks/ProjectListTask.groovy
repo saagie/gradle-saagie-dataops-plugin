82bcd1b452f41d7ff3010124426ae8bf6b7dd426
Renan Decamps <renan.decamps@gmail.com>
2019-07-03 11:25:33 +0200

    fix(CI)

--- src/main/groovy/io/saagie/plugin/dataops/tasks/ProjectListTask.groovy
+++ src/main/groovy/io/saagie/plugin/dataops/tasks/ProjectListTask.groovy
@@ -3,6 +3,7 @@ package io.saagie.plugin.dataops.tasks
 import io.saagie.plugin.dataops.DataOpsExtension
 import io.saagie.plugin.dataops.clients.SaagieClient
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 class ProjectListTask extends DefaultTask {
