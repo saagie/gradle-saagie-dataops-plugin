553e097ba720f1be47f87b0f0e51cdd2ab5b846c
Renan Decamps <renan.decamps@gmail.com>
2019-07-05 12:43:46 +0200

    feat: add unit tests and porjectListTechnologies task

--- src/main/groovy/io/saagie/plugin/dataops/tasks/ProjectListTechnologiesTask.groovy
+++ src/main/groovy/io/saagie/plugin/dataops/tasks/ProjectListTechnologiesTask.groovy
@@ -1,11 +1,17 @@
 package io.saagie.plugin.dataops.tasks
 
+import io.saagie.plugin.dataops.DataOpsExtension
+import io.saagie.plugin.dataops.clients.SaagieClient
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class ProjectListTechnologiesTask extends DefaultTask {
+    DataOpsExtension configuration
+    SaagieClient saagieClient
 
     @TaskAction
-
-
+    def getProjectTechnologies() {
+        saagieClient = new SaagieClient(configuration)
+        logger.quiet(saagieClient.getProjectTechnologies())
+    }
 }
