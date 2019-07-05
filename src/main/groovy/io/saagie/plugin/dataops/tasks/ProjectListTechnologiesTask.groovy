ad0361972f29752a4d9c47c07af6b31069aabc06
Renan Decamps <renan.decamps@gmail.com>
2019-07-05 09:43:48 +0200

    wip

--- src/main/groovy/io/saagie/plugin/dataops/tasks/ProjectListTechnologiesTask.groovy
+++ src/main/groovy/io/saagie/plugin/dataops/tasks/ProjectListTechnologiesTask.groovy
@@ -1,6 +1,11 @@
 package io.saagie.plugin.dataops.tasks
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
 
 class ProjectListTechnologiesTask extends DefaultTask {
+
+    @TaskAction
+
+
 }
