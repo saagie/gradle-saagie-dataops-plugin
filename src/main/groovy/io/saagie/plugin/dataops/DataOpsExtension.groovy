e8a798fd8c5af605e4064eb27ffce96c03f07fe8
Renan Decamps <renan.decamps@gmail.com>
2019-07-08 10:50:20 +0200

    feat: add request utils

--- src/main/groovy/io/saagie/plugin/dataops/DataOpsExtension.groovy
+++ src/main/groovy/io/saagie/plugin/dataops/DataOpsExtension.groovy
@@ -1,17 +1,29 @@
 package io.saagie.plugin.dataops
 
+import io.saagie.plugin.dataops.models.Job
+import io.saagie.plugin.dataops.models.JobVersion
 import io.saagie.plugin.dataops.models.Project
 import io.saagie.plugin.dataops.models.Server
 
 class DataOpsExtension {
     Server server = new Server()
     Project project = new Project()
+    Job job = new Job()
+    JobVersion jobVersion = new JobVersion()
 
-    Object server(Closure closure) {
+    Server server(Closure closure) {
         server.with(closure)
     }
 
-    Object project(Closure closure) {
+    Project project(Closure closure) {
         project.with(closure)
+    }
+
+    Job job(Closure closure) {
+        job.with(closure)
+    }
+
+    JobVersion jobVersion(Closure closure) {
+        jobVersion.with(closure)
     }
 }
