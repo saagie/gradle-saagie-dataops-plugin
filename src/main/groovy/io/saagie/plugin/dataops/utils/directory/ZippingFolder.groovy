7b83356da21803afb0c00e134be33838295d6b5e
amineziraoui <amine@bearstudio.fr>
2020-06-29 17:36:55 +0100

    Replaced zip from scratch code with zip4j lib

--- src/main/groovy/io/saagie/plugin/dataops/utils/directory/ZippingFolder.groovy
+++ src/main/groovy/io/saagie/plugin/dataops/utils/directory/ZippingFolder.groovy
@@ -1,11 +1,9 @@
 package io.saagie.plugin.dataops.utils.directory
 
+import net.lingala.zip4j.ZipFile
 import org.gradle.api.GradleException
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
-
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 class ZippingFolder {
     static final Logger logger = Logging.getLogger(ZippingFolder.class)
@@ -21,56 +19,33 @@ class ZippingFolder {
 
     void generateZip(File tempFile) throws IOException{
         try{
-            FileOutputStream fos = new FileOutputStream(zipFileName)
-            ZipOutputStream zipOut = new ZipOutputStream(fos)
-            File fileToZip = new File(inputDir)
-            zipFile(fileToZip, fileToZip.getName(), zipOut)
-            zipOut.close()
-            fos.close()
+            new ZipFile(zipFileName).addFolder(new File(inputDir))
+            logger.debug("Temporary file cache name : ${tempFile.name}")
             if(isNotDefaultTemp){
                 deleteInsideDirectory(tempFile)
             } else {
-                tempFile.deleteDir()
+                boolean isDeletedTmpFile = tempFile.deleteDir()
+                if(!isDeletedTmpFile) {
+                    throw new GradleException("One if the files didn t get deleted. File name: ${tempFile.name}")
+                }
             }
         } catch (IOException ex) {
             throw new GradleException(ex.message)
         }
     }
 
-    private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
-        if (fileToZip.isHidden()) {
-            return;
-        }
-        if (fileToZip.isDirectory()) {
-            if (fileName.endsWith("/")) {
-                zipOut.putNextEntry(new ZipEntry(fileName))
-                zipOut.closeEntry()
-            } else {
-                zipOut.putNextEntry(new ZipEntry(fileName + "/"))
-                zipOut.closeEntry()
-            }
-            File[] children = fileToZip.listFiles();
-            for (File childFile : children) {
-                zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
-            }
-            return;
-        }
-        FileInputStream fis = new FileInputStream(fileToZip)
-        ZipEntry zipEntry = new ZipEntry(fileName)
-        zipOut.putNextEntry(zipEntry)
-        byte[] bytes = new byte[1024];
-        int length
-        while ((length = fis.read(bytes)) >= 0) {
-            zipOut.write(bytes, 0, length)
-        }
-        fis.close()
-    }
-
     static deleteInsideDirectory(File folder) {
         String[]entries = folder.list();
+
         for(String s: entries){
+            Boolean isDeleted = true
             File currentFile = new File(folder.getPath(),s);
-            currentFile.deleteDir();
+            isDeleted = currentFile.deleteDir();
+            if(!isDeleted) {
+                throw new GradleException("One if the files didn t get deleted. File name: ${s}. Path name: ${folder.getPath()}")
+            } else {
+                logger.debug("Deleted custom tmp file : ${s} in path : ${folder.getPath()}")
+            }
         }
     }
 }
