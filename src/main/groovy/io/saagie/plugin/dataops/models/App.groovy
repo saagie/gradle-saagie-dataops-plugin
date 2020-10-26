a1be3668a4953f0886bfba53d148e7a417381c29
Zouhair Mkassmi <zouhair@bearstudio.fr>
2020-10-26 12:41:38 +0100

    fix app exportion and importation with adding unit test

--- src/main/groovy/io/saagie/plugin/dataops/models/App.groovy
+++ src/main/groovy/io/saagie/plugin/dataops/models/App.groovy
@@ -2,10 +2,13 @@ package io.saagie.plugin.dataops.models
 
 class App extends Job {
     int storageSizeInMB;
+    Boolean isStreaming;
+
     @Override
     Map toMap() {
         return [*              : super.toMap(),
-                storageSizeInMB: storageSizeInMB
+                storageSizeInMB: storageSizeInMB,
+                isStreaming : isStreaming
         ]
     }
 }
